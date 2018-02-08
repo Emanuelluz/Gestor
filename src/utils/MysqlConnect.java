f63c3acf2c52040b5c27429a234ef8d36ced6317
emanuel <emanuel@emanuel-Inspiron-5420>
2018-02-08 18:57:21 -0200

    0.2

---
+++ src/utils/MysqlConnect.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import model.*;
+import java.sql.*;
+
+/**
+ *
+ * @author emanu
+ */
+public class MysqlConnect {
+
+    Connection conn = null;
+
+    public Connection open() {
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:Gestor");
+            return conn;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public void close() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
